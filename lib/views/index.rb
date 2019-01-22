cc42f962ba6f357ff7ee3948f7b957ee837388ba
lebonvieuxlion <debaugelion@gmail.com>
2019-01-22 22:18:28 +0100

    Final touch

--- lib/views/index.rb
+++ lib/views/index.rb
@@ -19,7 +19,7 @@ class Index
 
 	def offer_choices_to_user
 
-		puts "Salut ptit chef. Tu veux scrapper des emaisl de mairies comme un fou ? Fais un choix pour définir comment tu vas entreposer tes données :"
+		puts "Salut ptit chef. Tu veux scrapper des emails de mairies comme un fou ? Fais un choix pour définir comment tu vas entreposer tes données :"
 		puts "Choisis 1 pour JSON "
 		puts "Choisis 2 pour Spreadsheets"
 		puts "Choisis 3 pour CSV"
